ebe7be4f930610fdbaf713d647b62b13945278ac
raezovic <paulsedwin8@gamil.com>
2024-06-11 15:52:10 +0300

    third landing commit

--- app/src/main/java/com/example/remoteapplication/LandpageActivity.java
+++ app/src/main/java/com/example/remoteapplication/LandpageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class LandpageActivity extends AppCompatActivity {
@@ -13,7 +14,7 @@ public class LandpageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landpage);
 
-        ImageButton btn_get_started = findViewById(R.id.btn_get_started);
+        Button btn_get_started = findViewById(R.id.getstarted_button);
         btn_get_started.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
